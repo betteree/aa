fd8516f496106a28bc0ae8863f996d64b4d53766
나은 <kkne1017@gmail.com>
2024-03-24 23:46:22 +0900

    뷰만들기

--- test/dataform.py
+++ test/dataform.py
@@ -1,6 +1,39 @@
 
-request=( 
-    userType : "patient",
-    
+request={
+    "header":{
+        "usertype": "patient",
+        "datetime": "YY/MM/DD",
+        "version": "0.1.0",
+        "request" : "login"
+    },
+    "body":{
+        "name":"김나은",
+        "guardNumber": "01030236298",
+        "patientNumber":"01011235677",
+        "address":"대구광역시",
+        "id":"adfadf",
+        "pw":"dafdsfa",
+        "xAddress":"133.214",
+        "yAddress":"123.124"
+    }
+}
 
-)
+response={
+    "header":{
+        "usertype": "patient",
+        "datetime": "YY/MM/DD",
+        "version": "0.1.0",
+        "request" : "login"
+    },
+    "body":{
+        "name":"김나은",
+        "address":"대구광역시",
+        "guardNumber": "01030236298",
+        "picture": ".url",
+        "xAddress":"133.214",
+        "yAddress":"123.124",
+        "check":"check",
+        "state":"login",
+        "detail":"오류"
+    }
+}
