81241fa201e6070f825c35767f7a0b15052c0397
나은 <kkne1017@gmail.com>
2024-04-29 22:40:14 +0900

    로그인컨트롤러 구현

--- test/test1.py
+++ test/test1.py
@@ -91,17 +91,17 @@ class Chatting_room():
     def get_pid(self):
         return self.__pid
     
-    #큐에 값 넣어주기 
+   #큐에 값 넣어주기 
     def is_queue_empty(self,):
         result = self.__send_que.empty()
         return result
 
-        
+       
     #큐에서 값 빼기
     def dequeue(self,):
         data = self.__recv_que.get()
         return data
-    
+   
     #큐에서 값 빼기
     def enqueue(self, data):
         self.__send_que.put(data)
@@ -112,7 +112,41 @@ class Chatting_room():
 class LoginController():
 
 
-    def sign_up(self,id,):
+    def __init__(self):
+        self.id_list = []
+        self.pw_list = []
+        self.name_list = []
+        self.patient_number = []
+        self.guardian_number = []
+        self.address_list = []
+        
+    def sign_up(self,data:dict):
+        id_value = data.get('id')
+        pw_value = data.get('pw')
+        name_value = data.get('name')
+        patient_number_value = data.get('patient_number')
+        guardian_number_value = data.get('guardian_number')
+        address_value = data.get('address')
 
-    def login():
+
+    if id_value and pw_value and name_value and patient_number_value and guardian_number_value and address_value:
+            self.id_list.append(id_value)
+            self.pw_list.append(pw_value)
+            self.name_list.append(name_value)
+            self.patient_number.append(patient_number_value)
+            self.guardian_number.append(guardian_number_value)
+            self.address_list.append(address_value)
+    else:
+        print("값을 모두 입력 바람")                       
+        
     
+    def login(self, id_value, pw_value):
+        if id_value in self.id_list:
+            index = self.id_list.index(id_value)
+            if self.pw_list[index] == pw_value:
+                print("로그인 성공")
+            else:
+                print("로그인 실패")
+
+        else:
+            print("아이디없음")
